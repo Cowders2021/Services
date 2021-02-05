a2e338f0dc0f14e9f31a38b29f9d043d0ea18e3d
Gunnar Thorvaldsen Abrahamsrud <Gunnar.Thorvaldsen.Abrahamsrud@felleskjopet.no>
2021-02-05 21:16:16 +0100

    SignalR

--- CowdersPictures/Startup.cs
+++ CowdersPictures/Startup.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using SignalR.Mvc;
 
 namespace CowdersPictures
@@ -26,6 +32,52 @@ namespace CowdersPictures
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(); // Make sure you call this previous to AddMvc
+
+            services.AddSignalR()
+                .AddAzureSignalR();
+
+            //services.AddAuthentication(options =>
+            //    {
+            //        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            //        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            //        options.DefaultChallengeScheme = "Vipps";
+            //    })
+            //    .AddCookie()
+            //    .AddOAuth("Vipps", options =>
+            //    {
+            //        options.ClientId = Configuration["Vipps:ClientId"];
+            //        options.ClientSecret = Configuration["Vipps:ClientSecret"];
+            //        options.CallbackPath = new PathString("/signin-vipps");
+
+            //        options.AuthorizationEndpoint = "'https://apitest.vipps.no/access-management-1.0/access/oauth2/auth";
+            //        options.TokenEndpoint = "'https://apitest.vipps.no/access-management-1.0/access/oauth2/token";
+            //        options.UserInformationEndpoint = "https://apitest.vipps.no/vipps-userinfo-api/userinfo";
+
+            //        //options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+            //        //options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+            //        //options.ClaimActions.MapJsonKey("urn:vipps:login", "login");
+            //        //options.ClaimActions.MapJsonKey("urn:vipps:url", "html_url");
+            //        //options.ClaimActions.MapJsonKey("urn:vipps:avatar", "avatar_url");
+
+            //        options.Events = new OAuthEvents
+            //        {
+            //            OnCreatingTicket = async context =>
+            //            {
+            //                var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+            //                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+            //                var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+            //                response.EnsureSuccessStatusCode();
+
+            //                var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            //                context.RunClaimActions();
+            //                //context.RunClaimActions(user);
+            //            }
+            //        };
+            //    }
+            //);
 
             services.AddControllers();
             // Register the Swagger generator, defining 1 or more Swagger documents
@@ -66,7 +118,6 @@ namespace CowdersPictures
             });
 
             app.UseRouting();
-
 
             app.UseAuthorization();
 
