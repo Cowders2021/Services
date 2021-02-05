4a33033f083809b980bb6d41055a0d6f878c05a1
Gunnar Thorvaldsen Abrahamsrud <Gunnar.Thorvaldsen.Abrahamsrud@felleskjopet.no>
2021-02-05 08:31:33 +0100

    Added Issues API and Swagger

---
+++ CowdersPictures/Controllers/Issues.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CowdersPictures.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CowdersPictures.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Issues : ControllerBase
+    {
+
+        internal static IEnumerable<Issue> _issuesList = new List<Issue>
+            {
+                new Issue {Id = 1, CustomerId = 42,  ProductId = 10006, Text = "Ugress",  Desctiption = "Mye ugress i mellomhøyde nær skog som heller i østlig retning"},
+                new Issue {Id = 2, CustomerId = 42,  ProductId = 10006, Text = "Ugress her også!!!",  Desctiption = "Mye ugress når jeg myser lett mot sola i fullmåne"}
+            };
+
+        // GET: api/<Issues>
+        [HttpGet]
+        public IEnumerable<Issue> Get()
+        {
+            return _issuesList;
+        }
+
+        // GET api/<Issues>/5
+        [HttpGet("{id}")]
+        public Issue Get(int id)
+        {
+            return _issuesList.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/<Issues>
+        [HttpPost]
+        public void Post([FromBody] Issue value)
+        {
+        }
+
+        // PUT api/<Issues>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Issue value)
+        {
+        }
+
+        // DELETE api/<Issues>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- CowdersPictures/Startup.cs
+++ CowdersPictures/Startup.cs
@@ -26,6 +26,8 @@ namespace CowdersPictures
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +39,23 @@ namespace CowdersPictures
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Issues API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
 
             app.UseRouting();
 
