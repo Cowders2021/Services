1146b220a0a57d46532188886a2e8fcc2d64894d
Gunnar Thorvaldsen Abrahamsrud <Gunnar.Thorvaldsen.Abrahamsrud@felleskjopet.no>
2021-02-06 02:07:41 +0100

    Forgott a file

---
+++ CowdersPictures/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CowdersPictures.Dtos;
+using CowdersPictures.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CowdersPictures
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Add as many of these lines as you need to map your objects
+            CreateMap<IssueEntity, Issue>();
+            CreateMap<Issue, IssueEntity>();
+            CreateMap<Sensor, SensorEntity>();
+            CreateMap<SensorEntity, Sensor>();
+        }
+    }
+}
